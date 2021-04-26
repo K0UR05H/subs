dabff65ca14d70637dc20720e8d5480464088fd6
k0ur0x <kourox@protonmail.com>
2021-04-26 06:38:21 +0430

    Support stdin

--- subfind/src/main.rs
+++ subfind/src/main.rs
@@ -2,15 +2,16 @@ use ansi_term::Color::{Blue, Green, Red};
 use clap::{App, Arg};
 use regex::Regex;
 use std::{
-    env,
-    error::Error,
+    env, error,
     ffi::OsStr,
     fs::{self, File},
+    io::{self, Read},
     path::Path,
+    result,
 };
 use subtitles::SubRip;
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
+type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -18,18 +19,11 @@ const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
 const ABOUT: &str = env!("CARGO_PKG_DESCRIPTION");
 
 mod options {
-    pub const DIR: &str = "directory";
+    pub const PATH: &str = "path";
     pub const PATTERN: &str = "pattern";
-    pub const RECURSIVE: &str = "recursive";
 }
 
 fn main() -> Result<()> {
-    let current_dir = env::current_dir()?;
-    let current_dir = match current_dir.to_str() {
-        Some(path) => path,
-        None => panic!("could not get the current working directory."),
-    };
-
     let matches = App::new(NAME)
         .version(VERSION)
         .author(AUTHOR)
@@ -37,65 +31,68 @@ fn main() -> Result<()> {
         .arg(
             Arg::with_name(options::PATTERN)
                 .value_name("PATTERN")
+                .help("pattern to search for")
                 .required(true)
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name(options::DIR)
-                .short("d")
-                .long("directory")
-                .value_name("DIR")
-                .help("set a directory to search for the subtitles")
-                .default_value(current_dir)
-                .hide_default_value(true),
-        )
-        .arg(
-            Arg::with_name(options::RECURSIVE)
-                .short("r")
-                .long("recursive")
-                .help("search recursively"),
+            Arg::with_name(options::PATH)
+                .value_name("PATH")
+                .help("subtitles path (standard input by default)")
+                .default_value("-")
+                .hide_default_value(true)
+                .multiple(true),
         )
         .get_matches();
 
     let pattern = matches.value_of(options::PATTERN).unwrap();
-    let path = matches.value_of(options::DIR).unwrap();
-    let recursive = matches.is_present(options::RECURSIVE);
     let regex = Regex::new(pattern)?;
+    let paths = matches.values_of(options::PATH).unwrap();
 
-    subfind(&regex, path, recursive)
-}
-
-fn subfind(regex: &Regex, path: impl AsRef<Path>, recursive: bool) -> Result<()> {
-    let entries = fs::read_dir(path)?;
-
-    for entry in entries {
-        let entry = entry?;
-        let file_type = entry.file_type()?;
-
-        if file_type.is_dir() && recursive {
-            subfind(regex, entry.path(), true)?;
-        } else if file_type.is_file() {
-            find(regex, &entry.path())?;
+    for path in paths {
+        if path == "-" {
+            find_in_stdin(&regex);
+        } else {
+            find_in_path(&regex, path)?;
         }
     }
 
     Ok(())
 }
 
-fn find(regex: &Regex, path: &Path) -> Result<()> {
-    print_file_name(path);
+fn find_in_stdin(regex: &Regex) {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+    find_matches(&regex, handle);
+}
 
-    let file = File::open(path)?;
-    let parser = subtitles::open(file);
+fn find_in_path(regex: &Regex, path: impl AsRef<Path>) -> Result<()> {
+    let file_type = fs::metadata(&path)?.file_type();
+
+    if file_type.is_dir() {
+        let entries = fs::read_dir(&path)?;
+        for entry in entries {
+            let entry = entry?;
+            find_in_path(regex, entry.path())?;
+        }
+    } else if file_type.is_file() {
+        print_file_name(path.as_ref());
+        let file = File::open(path)?;
+        find_matches(regex, file);
+    }
+
+    Ok(())
+}
+
+fn find_matches<T: Read>(regex: &Regex, subtitle: T) {
+    let parser = subtitles::open(subtitle);
 
     for entry in parser {
         match entry {
-            Ok(subtitle) => print_match(subtitle, regex),
+            Ok(sub) => print_matches(sub, regex),
             Err(err) => eprintln!("{}: {}", Red.paint("Error"), err),
         }
     }
-
-    Ok(())
 }
 
 fn print_file_name(path: &Path) {
@@ -108,7 +105,7 @@ fn print_file_name(path: &Path) {
     println!("{}", Blue.paint(file_name));
 }
 
-fn print_match(subtitle: SubRip, regex: &Regex) {
+fn print_matches(subtitle: SubRip, regex: &Regex) {
     for line in subtitle.text {
         if let Some(matched) = regex.find(&line) {
             println!(
